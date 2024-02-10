b2a9375d5d0fe224bf9ad3b4c28552ed5ea99085
OsternHanz <126981204+OsternHanz@users.noreply.github.com>
2024-02-10 04:37:40 +0300

    Add files via upload

---
+++ src/module2.py
@@ -0,0 +1,11 @@
+import torch
+
+w=torch.tensor([[5., 10.],[1., 2.]], requires_grad=True)
+optimizer=torch.optim.SGD([w], lr=0.001)
+for i in range(500):
+    function=torch.prod(torch.log(torch.log(w+7)))
+    function.backward()
+    optimizer.step()
+    optimizer.zero_grad()
+
+print(w)
